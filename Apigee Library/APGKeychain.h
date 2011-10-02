647658b3db9653b26a36dfd4c5c0e6454edad4b7
Mike Mayo <greenisus@gmail.com>
2011-10-02 16:15:07 -0700

    improve documentation

--- Apigee Library/APGKeychain.h
+++ Apigee Library/APGKeychain.h
@@ -1,19 +1,29 @@
 #import <Foundation/Foundation.h>
 
 /**
+ The APGKeychain class is a convenience class for securely persisting and retrieving
+ strings from the system keychain.
  */
 @interface APGKeychain : NSObject {
 }
 
 /**
+ Securely persists a string with the key you provide.
+ Example: [APGKeychain setString:@"secret" forKey:@"smartKey"];
+ @return BOOL for whether or not the string was successfully persisted
  */
 + (BOOL)setString:(NSString *)string forKey:(NSString *)key;
 
 /**
+ Retrieves a string with the key you provide.
+ Example: NSString *smartKey = [APGKeychain stringForKey:@"smartKey"];
+ @return NSString the string in the keychain
  */
 + (NSString *)stringForKey:(NSString *)key;
 
 /**
+ Retrieves a previously persisted string from the keychain.
+ @return BOOL for whether or not the delete was successful
  */
 + (BOOL)deleteStringForKey:(NSString *)key;
 
--- Apigee Library/APGLoginViewController.h
+++ Apigee Library/APGLoginViewController.h
@@ -10,18 +10,30 @@
 #import <UIKit/UIKit.h>
 
 /**
+ The APGLoginViewController class is a convenience class for iOS apps to display
+ the OAuth login page for other providers such as Twitter.  You can show the view
+ with the -(void)presentLoginForProvider:fromViewController: method in APGClient,
+ or you can subclass it and present it yourself if you would like to override the
+ appearance of the view.  See APGClient.m for proper setup.  Also, your iOS app
+ should be configured to handle a custom URL type.  See the README or Apple documentation
+ for instructions.
+ This class is not available for Mac applications.
+ @see APGClient
  */
 @interface APGLoginViewController : UIViewController <UIWebViewDelegate>
 
 /**
+ The URL for the login page
  */
 @property (nonatomic, retain) NSURL *url;
 
 /**
+ The web view for the login page
  */
 @property (nonatomic, retain) UIWebView *webView;
 
 /**
+ Creates and returns an APGLoginViewController that will load the URL
  */
 - (id)initWithURL:(NSURL *)url;
 
